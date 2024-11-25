bc745b2b8a14162faba2ee11e90356aa2d96f2f7
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2024-11-25 21:51:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os, sys
 filename = sys.argv([1])
-executableCode = ""
+executableCode = "import os, sys, subprocess, shutil"
 file = open(filename, "r")
 code = file.Read()
 file.close()
@@ -11,7 +11,23 @@ file = open(filename, "r")
 codeLinesLen = len(file.ReadLines())
 for i in range(0, codeLinesLen):
   codeLine = file.ReadLine(i)
-  for j in range(0, len(codeLine)):
-    codeLine[j]
+    if codeLine.endswith("["):
+      if codeLine == "EVP(CDLoop)[":
+        executableCode += "subprocess.call(\"cscript VBS\\CDLoop.vbs\")"
+      if codeLine.startswith("EVP(SendKey(\"") and codeLine.endswith("))"):
+        for j in range(13, len(codeLine)):
+          if codeLine[j] not in ListForSlash:
+
+          else:
+            print("PythonError: in {codeLine}, need symbol '\' before writing
+      else:
+        print(f"UnexpectedFuncError: in {codeLine}")
+    else:
+      print(f"SyntaxError: No end signal of line signal in {codeLine} ([)")
+    
+  
+    
 if __name__ == "__main__":
+  file.close()
+  os.remove(file)
   exec(executableCode)
